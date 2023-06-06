814c213da279380534550c89290d15b600fd2e47
Bbaktybek5 <baktybekba1mtoff701@gmail.com>
2023-06-06 17:49:48 +0600

    check internet

--- app/src/main/java/com/example/myhomework4/ui/MainActivity.kt
+++ app/src/main/java/com/example/myhomework4/ui/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.myhomework4.ui
 
 import android.content.Intent
+import android.view.View
 import androidx.lifecycle.ViewModelProvider
 import com.example.myhomework4.base.BaseActivity
 import com.example.myhomework4.databinding.ActivityMainBinding
 import com.example.myhomework4.model.DetailActivity
 import com.example.myhomework4.model.Playlists
 import com.example.myhomework4.ui.adapter.PlayListAdapter
+import com.example.myhomework4.utils.ConnectionLiveData
 
 
 class MainActivity() : BaseActivity<ActivityMainBinding, MainViewModel>() {
@@ -30,16 +32,32 @@ class MainActivity() : BaseActivity<ActivityMainBinding, MainViewModel>() {
         }
     }
 
+    override fun checkInternet() {
+        super.checkInternet()
+        ConnectionLiveData(application).observe(this) {
+            if (it) {
+                binding.internetConnection.visibility = View.VISIBLE
+                binding.noConnection.visibility = View.GONE
+            } else {
+                binding.internetConnection.visibility = View.GONE
+                binding.noConnection.visibility = View.VISIBLE
+                setupLiveData()
+            }
+        }
+    }
+
     override fun inflateViewBinding(): ActivityMainBinding {
         return ActivityMainBinding.inflate(layoutInflater )
     }
+
 
     private fun onClick(item: Playlists.Item){
         val intent = Intent(this, DetailActivity::class.java)
         intent.putExtra(ID, item.id)
         startActivity(intent)
-
     }
+
+
     companion object{
         const val ID = "ID"
     }
